bcbc741055ec07aed7f106e8e98e33bfe1db61ca
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-24 00:18:11 +0800

    wt88047: init: Initial O Bringup
    Change-Id: Ica17ee73a5f7503a3b6d109139950109c8b53e65

--- init/init_wt88047.cpp
+++ init/init_wt88047.cpp
@@ -2,8 +2,7 @@
    Copyright (c) 2016, The Linux Foundation. All rights reserved.
    Copyright (C) 2016, The CyanogenMod Project
    Copyright (C) 2015-2016, Ketut P. Kumajaya
-   Copyright (C) 2016-2017, Nikolai Petrenko
-   Copyright (C) 2017, The LineageOS Project
+   Copyright (C) 2017-2018, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -38,12 +37,17 @@
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
 
-#include "vendor_init.h"
+#include <android-base/properties.h>
+
 #include "property_service.h"
-#include "log.h"
+#include "vendor_init.h"
+
 #include "util.h"
 
 #include "init_msm8916.h"
+
+using android::base::GetProperty;
+using android::base::SetProperty;
 
 void property_override(char const prop[], char const value[])
 {
@@ -67,9 +71,10 @@ void init_target_properties()
 {
     std::ifstream fin;
     std::string buf;
+    std::string product;
 
-    std::string product = property_get("ro.product.name");
-    if (product.find("wt88047") == std::string::npos)
+    product = GetProperty("ro.product.name", "");
+    if (product != "wt88047")
         return;
 
     fin.open("/proc/cmdline");
@@ -92,7 +97,7 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014819");
         property_override("ro.product.model", "2014819");
         property_override("ro.product.name", "2014819");
-        property_set("ro.telephony.default_network", "9,1");
+        SetProperty("ro.telephony.default_network", "9,1");
     }
     /* S88047C1 */
     else if (buf.find("S88047C1") != std::string::npos) {
@@ -100,9 +105,9 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014818");
         property_override("ro.product.model", "2014818");
         property_override("ro.product.name", "2014818");
-        property_set("ro.telephony.default_network", "9,1");
-        property_set("persist.dbg.volte_avail_ovr", "1");
-        property_set("persist.dbg.vt_avail_ovr", "1");
+        SetProperty("ro.telephony.default_network", "9,1");
+        SetProperty("persist.dbg.volte_avail_ovr", "1");
+        SetProperty("persist.dbg.vt_avail_ovr", "1");
     }
     /* S88047B2 */
     else if (buf.find("S88047B2") != std::string::npos) {
@@ -110,9 +115,9 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014821");
         property_override("ro.product.model", "2014821");
         property_override("ro.product.name", "2014821");
-        property_set("ro.telephony.default_network", "22,1");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("persist.radio.sglte.eons_domain", "ps");
+        SetProperty("ro.telephony.default_network", "22,1");
+        SetProperty("telephony.lteOnCdmaDevice", "1");
+        SetProperty("persist.radio.sglte.eons_domain", "ps");
     }
     /* S88047B1 */
     else if (buf.find("S88047B1") != std::string::npos) {
@@ -120,9 +125,9 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014812");
         property_override("ro.product.model", "2014812");
         property_override("ro.product.name", "2014812");
-        property_set("ro.telephony.default_network", "22,1");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("persist.radio.sglte.eons_domain", "ps");
+        SetProperty("ro.telephony.default_network", "22,1");
+        SetProperty("telephony.lteOnCdmaDevice", "1");
+        SetProperty("persist.radio.sglte.eons_domain", "ps");
     }
     /* S86047A1 and S86047A1_CD */
     else if (buf.find("S86047A1") != std::string::npos) {
@@ -130,7 +135,7 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014813");
         property_override("ro.product.model", "2014813");
         property_override("ro.product.name", "2014813");
-        property_set("ro.telephony.default_network", "9,1");
+        SetProperty("ro.telephony.default_network", "9,1");
     }
     /* S86047A2 and S86047A2_CD */
     else if (buf.find("S86047A2") != std::string::npos) {
@@ -138,7 +143,7 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014112");
         property_override("ro.product.model", "2014112");
         property_override("ro.product.name", "2014112");
-        property_set("ro.telephony.default_network", "9,1");
+        SetProperty("ro.telephony.default_network", "9,1");
     }
     /* S88047A2 and S88047A1 */
     else {
@@ -146,13 +151,13 @@ void init_target_properties()
         property_override("ro.product.device", "HM2014811");
         property_override("ro.product.model", "2014811");
         property_override("ro.product.name", "2014811");
-        property_set("ro.telephony.default_network", "9,1");
+        SetProperty("ro.telephony.default_network", "9,1");
     }
 
-    property_set("dalvik.vm.heapstartsize", "8m");
-    property_set("dalvik.vm.heapgrowthlimit", is2GB() ? "192m" : "96m");
-    property_set("dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", is2GB() ? "512k" : "2m");
-    property_set("dalvik.vm.heapmaxfree", "8m");
+    SetProperty("dalvik.vm.heapstartsize", "8m");
+    SetProperty("dalvik.vm.heapgrowthlimit", is2GB() ? "192m" : "96m");
+    SetProperty("dalvik.vm.heapsize", is2GB() ? "512m" : "256m");
+    SetProperty("dalvik.vm.heaptargetutilization", "0.75");
+    SetProperty("dalvik.vm.heapminfree", is2GB() ? "512k" : "2m");
+    SetProperty("dalvik.vm.heapmaxfree", "8m");
 }
